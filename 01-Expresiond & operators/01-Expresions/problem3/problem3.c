a3437e29ca24d8341f10387d06e926cf5f2b2b27
Ahmed Mostafa El Gendy <121825618+Ahmed-Mostafa-El-Gendy@users.noreply.github.com>
2024-02-19 08:31:02 +0200

    Add problem3

---
+++ 01-Expresiond & operators/01-Expresions/problem3/problem3.c
@@ -0,0 +1,25 @@
+/*
+ ******************************************************************************
+ * @file           : problem3 
+ * @author         : Ahmed_Mostafa_El_Gendy
+ * @brief          : swap two variables 
+ * @date           : 19 / 2 / 2024 
+ ******************************************************************************
+*/ 
+#include<stdio.h>
+int main ()
+{
+	int var1 , var2 , temp = 0 ;
+	printf("please enter the var1 = ");
+	scanf("%d",&var1);
+	printf("please enter the var2  = ");
+	scanf("%d",&var2);
+	printf("the var1 before swap = %d \n" ,var1);
+	printf("the var2 before swap = %d \n ",var2);
+	temp = var1 ;
+	var1 = var2 ;
+	var2 = temp ;
+	printf("the var1 after swap = %d \n ",var1);
+	printf("the var2 after swap = %d \n ",var2);
+	
+}
