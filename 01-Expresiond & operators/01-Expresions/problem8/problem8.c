5d7470b19a674f2fbefabd42e5aad0da36b5f985
Ahmed Mostafa El Gendy <121825618+Ahmed-Mostafa-El-Gendy@users.noreply.github.com>
2024-02-23 19:03:47 +0200

    problem8/

---
+++ 01-Expresiond & operators/01-Expresions/problem8/problem8.c
@@ -0,0 +1,16 @@
+/*
+ ******************************************************************************
+ * @file           : problem8
+ * @author         : Ahmed_Mostafa_El_Gendy
+ * @brief          : write a code to print your name and other info 
+ * @date           : 23/2/ 2024 
+ ******************************************************************************
+*/ 
+#include<stdio.h>
+int main ()
+{
+	printf("my name is Ahmed Mostafa El Gendy \n ");
+	printf("my birth day is 4 / 12 / 2001  \n ");
+	printf("my phone number is 01140643418  \n ");
+	
+}
